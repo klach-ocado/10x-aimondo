cb610eacf58832bb54a641432f97a66977d9b861
Krzysztof Lach <krzysztof.lach@ocado.com>
2025-10-16 15:47:07 +0200

    chore: install and setup Playwright

---
+++ e2e/example.spec.ts
@@ -0,0 +1,8 @@
+import { test, expect } from '@playwright/test';
+
+test('has login form', async ({ page }) => {
+  await page.goto('/auth/login');
+
+  // Expect a title "to contain" a substring.
+  await expect(page.getByTestId('login-form')).toBeVisible();
+});
